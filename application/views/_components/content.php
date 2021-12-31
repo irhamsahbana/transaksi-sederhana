058a7fd86812609b7956eb56bd20e9cf378accd6
irhamsahbana <irhamsahbana@gmail.com>
2021-12-31 23:56:53 +0800

    slicing template

---
+++ application/views/_components/content.php
@@ -0,0 +1,4 @@
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <?php if($isi) $this->load->view($isi); ?>
+  </div>
---
+++ application/views/_components/wrapper.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php  require_once 'head.php'; ?>
+
+    <body class="hold-transition sidebar-mini layout-fixed">
+        <div class="wrapper">
+            <?php  require_once 'preloader.php'; ?>
+            <?php  require_once 'navbar.php'; ?>
+            <?php  require_once 'sidebar.php'; ?>
+
+            <?php  require_once 'content.php'; ?>
+
+            <?php require_once 'footer.php'; ?>
+        </div>
+
+        <?php  require_once 'script.php'; ?>
+    </body>
+
+</html>
